65b078209582ec4a93bf950373379410572c416a
mohamad-opm <mohamab69@ac.sce.ac.il>
2026-04-17 19:53:21 +0300

    fix deploy

--- app.py
+++ app.py
@@ -179,4 +179,7 @@ def reset_password(username):
     return render_template('reset_password.html', username=username)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
